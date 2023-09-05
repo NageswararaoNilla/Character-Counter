13146d21b5abbbb86d9b16541a57e64d8de201b4
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-09-05 16:04:04 +0000

    project completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import CharacterCounter from './components/CharacterCounter'
+
 import './App.css'
 
-// Replace your code here
-const App = () => <h1>Hello, World</h1>
+const App = () => <CharacterCounter />
 
 export default App
